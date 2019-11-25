230f785bd450e0235b64e6da958d0aef5f6a1dc4
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2019-11-25 17:09:29 +0530

    Freshers

---
+++ src/Components/Topbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+class TopBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            AlgorithmList = []
+        }
+    }
+    render(){
+        
+    }
+}
