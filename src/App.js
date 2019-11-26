b11f1c947a338be0c43bbb2ccd615b77d0e65f7e
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2019-11-26 19:13:34 +0530

    Adding topbar now

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
 import Grid from "./Components/Grid";
+import Topbar from "./Components/Topbar/index.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
+      <Topbar></Topbar>
       <Grid height={25} width={60} />
     </div>
   );
--- src/Components/Topbar/index.js
+++ src/Components/Topbar/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button
+} from "react-bootstrap";
 
-class TopBar extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            AlgorithmList = []
-        }
-    }
-    render(){
-        
-    }
+class TopBar extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //     AlgorithmList = []
+    // }
+  }
+  render() {
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <NavDropdown title="Algorithms" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">A *</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Djikstra</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">BFS</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">DFS</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
+
+export default TopBar;
